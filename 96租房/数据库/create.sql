a52d4387e85a11e11c2f12389b313d62655edadd
yuanhangs <444012836@qq.com>
2018-03-26 16:26:59 +0800

    96租房

---
+++ 96租房/数据库/create.sql
@@ -0,0 +1,42 @@
+﻿create table DISTRICT  (
+   ID                   int(4)                       not null AUTO_INCREMENT,
+   NAME                 varchar(50)                   not null,
+  PRIMARY KEY (`ID`)
+);
+
+create table HOUSE  (
+   ID                   int(6)                       not null AUTO_INCREMENT,
+   USER_ID              int(4),
+   TYPE_ID              int(4),
+   TITLE                varchar(50),
+   DESCRIPTION          varchar(2000),
+   PRICE                int(6),
+   PUBDATE              DATE,
+   FLOORAGE             int(4),
+   CONTACT              varchar(100),
+   STREET_ID            int(4),
+  PRIMARY KEY (`ID`)
+);
+
+create table STREET  (
+   ID                   int(4)                       not null AUTO_INCREMENT,
+   NAME                 varchar(50)                   not null,
+   DISTRICT_ID          int(4),
+  PRIMARY KEY (`ID`)
+);
+
+create table TYPES  (
+   ID                   int(4)                       not null AUTO_INCREMENT,
+   NAME                 varchar(50)                   not null,
+  PRIMARY KEY (`ID`)
+);
+
+create table USERS  (
+   ID                   int(4)                       not null AUTO_INCREMENT,
+   NAME                 varchar(50)                   not null,
+   PASSWORD             varchar(50)                   not null,
+   TELEPHONE            varchar(15),
+   USERNAME             varchar(50),
+   ISADMIN              varchar(5),
+  PRIMARY KEY (`ID`)
+);
