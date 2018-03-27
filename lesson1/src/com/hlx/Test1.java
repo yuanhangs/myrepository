3edfd90da161256337a477ebf8144b3f5b243e84
yuanhangs <444012836@qq.com>
2018-03-27 09:37:00 +0800

    java lesson1

---
+++ lesson1/src/com/hlx/Test1.java
@@ -0,0 +1,14 @@
+package com.hlx;
+
+public class Test1 {
+  public static void main(String[] args){
+//	  for(int i=1;i<100;i++){
+//	  System.out.println("CCCC");
+//	  }
+//	  System.out.println("CCCC");
+	  System.out.println("³¤µÃ");
+//	  System.out.println("CCCC");
+//	  System.out.println("CCCC");
+	  
+  }
+}
